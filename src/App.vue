b93bc54bcd06202955a1410d6149484d81e63f74
dhoniaridho <dhoniaridho@gmail.com>
2022-01-14 09:07:59 +0700

    improve preview before download

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import axios from 'axios';
 import {reactive} from 'vue';
 import Loading from './components/Loading.vue';
 
-const state = reactive({
+const state = reactive<any>({
   url: '',
   type: 'mp3',
   blobUrl: '',
@@ -16,16 +16,12 @@ const state = reactive({
     duration: '',
   },
 });
-async function HandleSubmit() {
+async function getMedata() {
   state.isLoading = true;
   try {
-    let {data: response} = await axios.get(`https://dhoniaridho-yt-download.herokuapp.com/api/stream?url=${state.url}&type=${state.type}`, {
-      responseType: 'blob',
-    });
     let {data: medata} = await axios.get(`https://dhoniaridho-yt-download.herokuapp.com/api/stream/medata?url=${state.url}`);
 
     state.medata = medata;
-    state.blobUrl = URL.createObjectURL(response);
     state.isSuccess = true;
     state.activeTabs = 'preview';
   } catch (e) {
@@ -36,7 +32,22 @@ async function HandleSubmit() {
   }
 }
 
-function downloadFile() {
+async function downloadFile() {
+  state.isLoading = true;
+  try {
+    let {data: response} = await axios.get(`https://dhoniaridho-yt-download.herokuapp.com/api/stream?url=${state.url}&type=${state.type}`, {
+      responseType: 'blob',
+    });
+    state.blobUrl = URL.createObjectURL(response);
+    state.activeTabs = 'download';
+  } catch (error) {
+    state.hasError = true;
+    state.isSuccess = false;
+  } finally {
+    state.isLoading = false;
+  }
+}
+function saveFile() {
   const link = document.createElement('a');
   link.download = `${state.medata.title}.${state.type}`;
   link.href = state.blobUrl;
@@ -49,17 +60,35 @@ function downloadFile() {
     <div class="bg-slate-400 border w-11/12 h-5/6 md:w-[40%] md:min-h-[60%] overflow-hidden rounded-lg bg-opacity-40 backdrop-blur-md backdrop-filter">
       <div class="w-full h-full flex justify-center flex-col text-center gap-6 py-8 px-5" v-if="!state.isLoading && state.activeTabs === 'home'">
         <h1 class="text-4xl font-extrabold tracking-tighter">Youtube Downloader</h1>
-        <form class="flex flex-col gap-5" @submit.prevent="HandleSubmit">
+        <form class="flex flex-col gap-5" @submit.prevent="getMedata">
           <input class="form-input" type="text" name="url" id="" v-model="state.url" />
-          <select v-model="state.type" class="focus:outline-none rounded-lg py-2 px-3">
-            <option value=""></option>
-            <option value="mp3">Audio Mp3</option>
-            <option value="mp4">Video</option>
-          </select>
           <button class="button-primary bg-rose-500" type="submit">submit</button>
         </form>
       </div>
-      <div class="w-full h-full flex flex-col" v-if="state.isSuccess && state.activeTabs === 'preview'">
+      <div class="w-full h-full flex flex-col text-center gap-6" v-if="!state.isLoading && state.activeTabs === 'preview'">
+        <div class="h-5 bg-white bg-opacity-30 w-full py-8 px-5 flex justify-between items-center">
+          <button @click="state.activeTabs = 'home'">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+          </button>
+        </div>
+        <div class="py-8 px-5">
+          <img class="mx-auto" :src="state.medata.thumbnails[2].url" alt="" />
+          <h2>{{ state.medata.title }}</h2>
+          <div class="py-8 px-5">
+            <form @submit.prevent="downloadFile" class="flex flex-col gap-5">
+              <select v-model="state.type" class="focus:outline-none rounded-lg py-2 px-3">
+                <option value=""></option>
+                <option value="mp3">Audio Mp3</option>
+                <option value="mp4">Video</option>
+              </select>
+              <button class="button-primary">{{ state.isLoading ? 'GET DATA' : 'DOWNLOAD' }}</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="w-full h-full flex flex-col" v-if="state.isSuccess && state.activeTabs === 'download'">
         <div class="h-5 bg-white bg-opacity-30 w-full py-8 px-5 flex justify-between items-center">
           <button @click="state.activeTabs = 'home'">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -75,7 +104,7 @@ function downloadFile() {
           <video class="max-h-[13rem]" v-if="state.type === 'mp4'" controls>
             <source :src="state.blobUrl" type="video/mp4" />
           </video>
-          <button @click="downloadFile" class="button-primary">DOWNLOAD</button>
+          <button @click="saveFile" class="button-primary">SAVE FIlE</button>
         </div>
       </div>
       <Loading v-if="state.isLoading" />
